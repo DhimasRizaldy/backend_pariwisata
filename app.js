5b84471cadd971a3000e9728a5343e7e3eeb8f9e
DhimasRizaldy <dtekchannel02@gmail.com>
2023-11-25 22:39:59 +0700

    dimas:deploy

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const app = express();
 const morgan = require('morgan');
 const { PORT } = process.env;
 const v1router = require('./routes/v1.routes');
-const vercel = require('@vercel/node');
 
 
 // middlewares
