d13a3563a9906be40fc15e933375c1f08e4c2186
DhimasRizaldy <dtekchannel02@gmail.com>
2023-11-25 22:35:04 +0700

    dimas:deploy

--- controllers/daerah.controllers.js
+++ controllers/daerah.controllers.js
@@ -1,5 +1,7 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+// const { PrismaClient } = require('@prisma/client');
+// const prisma = new PrismaClient();
+const { PrismaClient } = require('./prisma/generated/client');
+const prisma = new PrismaClient()
 const { getPagination } = require("../handler/pagination");
 
 module.exports = {
